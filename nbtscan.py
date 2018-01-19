4ed58b17636b5192d672756bd08f59839526f98c
Luong NGUYEN <luongnv89@gmail.com>
2018-01-19 17:49:57 +0100

    Rename and add some more function

--- nbtscan.py
+++ nbtscan.py
@@ -1,3 +1,4 @@
+# Need to install nbtscan
 import json
 import subprocess
 
@@ -13,7 +14,7 @@ class LiveNode:
         self.ip = ip
         self.mac = mac
 
-def nb_parse_line(line):
+def parse_nbtscan_data(line):
     """Parse an nbtscan line to get information of a node in network
     
     Arguments:
@@ -22,7 +23,7 @@ def nb_parse_line(line):
     values = line.split()
     return LiveNode(values[1].strip(),values[0],values[len(values)-1])
 
-def nb_parse_output(nb_output):
+def parse_nbtscan_output(nb_output):
     """Parse nbtscan output to get information in JSON format
 
     Arguments:
@@ -46,13 +47,13 @@ def nb_parse_output(nb_output):
     for line in lines:
         if (start_data == True and line != ""):
             # print(line + '\n')
-            new_node = nb_parse_line(line)
+            new_node = parse_nbtscan_data(line)
             all_nodes.append(new_node)
         if (line == "------------------------------------------------------------------------------"):
             start_data = True
     return all_nodes
 
-def nb_scan(scan_range):
+def get_all_LiveNode(scan_range):
     """Scan local network and return in JSON format the information of all device in the network.
     Each device has informations:
     - Name (hostname)
@@ -71,7 +72,45 @@ def nb_scan(scan_range):
     nbtscan = subprocess.Popen(["nbtscan",scan_range], stdout=subprocess.PIPE)
     scan_output = nbtscan.communicate()[0]
     # print(scan_output)
-    nodes = nb_parse_output(scan_output)
+    return parse_nbtscan_output(scan_output)
+
+def list_liveNode_to_json(nodes):
     return json.dumps([node.__dict__ for node in nodes])
 
-print (nb_scan("192.168.0.14"))
+def get_LiveNode_by_MAC(mac,scan_range):
+    """Get all LiveNode with given device's MAC address in a network range
+
+    Arguments:
+        mac {String} -- device MAC address
+        ip_range {String} -- what to scan. Can either be single IP
+			like 192.168.1.1 or
+			range of addresses in one of two forms: 
+			xxx.xxx.xxx.xxx/xx or xxx.xxx.xxx.xxx-xxx.
+    Return: we can have many LiveNode which have the same MAC address is: 000000000000
+    """
+    mac_node = []
+    nodes = get_all_LiveNode(scan_range)
+    for node in nodes:
+        if(node.mac == mac):
+            mac_node.append(node)
+    return mac_node
+
+def get_LiveNode_by_IP(ip):
+    """Get LiveNode which have given IP address in an IP range  
+    
+    Arguments:
+        ip {String} -- IP address
+    """
+    return get_all_LiveNode(ip)[0]
+
+nodes = get_all_LiveNode("192.168.0.14/26")
+print ("nodes:")
+print (list_liveNode_to_json(nodes))
+
+mac_nodes = get_LiveNode_by_MAC("60f81daaa622","192.168.0.14/26")
+print ("mac_nodes:")
+print (list_liveNode_to_json(nodes))
+
+ip_node = get_LiveNode_by_IP("192.168.0.20")
+print("IP node:")
+print (list_liveNode_to_json([ip_node]))
